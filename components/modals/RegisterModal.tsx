0e4d9c2e4a1ae9896bc6a4d55e9a71be0e198636
unknown <filip.trifunovic23@gmail.com>
2023-03-24 16:21:42 +0100

    adding toaster for success and error

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -1,8 +1,11 @@
 import useLoginModal from "@/hooks/useLoginModal";
 import useRegisterModal from "@/hooks/useRegisterModal";
+import axios from "axios";
 import { useCallback, useState } from "react";
+import toast from "react-hot-toast";
 import Input from "../Input";
 import Modal from "../Modal";
+import { signIn } from "next-auth/react";
 
 const RegisterModal = () => {
   const loginModal = useLoginModal();
@@ -25,13 +28,28 @@ const RegisterModal = () => {
     try {
       setIsLoading(true);
 
+      await axios.post("/api/register", {
+        email,
+        password,
+        username,
+        name,
+      });
+
+      toast.success("Account created.");
+
+      signIn("credentials", {
+        email,
+        password,
+      });
+
       registerModal.onClose();
     } catch (error) {
       console.log(error);
+      toast.error("Something went wrong.");
     } finally {
       setIsLoading(false);
     }
-  }, [registerModal]);
+  }, [registerModal, email, password, username, name]);
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
