fd8d76dad89b03c31cf88b9b778000200eb9d13b
unknown <filip.trifunovic23@gmail.com>
2023-03-27 15:03:18 +0200

    user profile page

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -25,7 +25,7 @@ const Avatar: React.FC<AvatarProps> = ({ userId, isLarge, hasBorder }) => {
     <div
       className={`${hasBorder ? "border-4 border-black" : ""} 
       ${isLarge ? "h-32" : "h-12"}
-      ${isLarge ? "h-32" : "w-12"}
+      ${isLarge ? "w-32" : "w-12"}
       rounded-full
       hover:opacity-90
       transition
