0a1698379c5db65ac591b9283ed7e9be63e0275b
unknown <filip.trifunovic23@gmail.com>
2023-03-29 14:15:30 +0200

    adding post feed for user

--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import PostFeed from "@/components/posts/PostFeed";
 import UserBio from "@/components/users/UserBio";
 import UserHero from "@/components/users/UserHero";
 import useUser from "@/hooks/useUser";
@@ -22,6 +23,7 @@ const UserView = () => {
       <Header showBackArrow label={fetchUser?.name} />
       <UserHero userId={userId as string} />
       <UserBio userId={userId as string} />
+      <PostFeed userId={userId as string} />
     </>
   );
 };
